a7b9141d82b939e2aafcd1e6558650527e53c1ed
vamshikrishnafive <saikrishana9@gmail.com>
2020-03-23 22:42:10 +0530

    added

---
+++ objects.js
@@ -0,0 +1,76 @@
+/*
+# Release 0: Objects Exercise
+- Finish all exercizes from [here](https://www.codecademy.com/courses/spencer-sandbox/0/1) till the 12th execize.
+you get more power to proceed to next. Time to gain all powers & become super-object-oriented-ninja
+*/
+/*
+# Release1:
+### 1. Create objects using the Object function
+The Object function provides a simple way to create a new instance of an object. After an object is created, it's just as easy to define new properties and methods. To do so, perform these tasks:
+
+- Create an object using the Object function.
+- Add a property to the new object.
+- Add a method to the new object.
+*/
+function myObject(a,b){
+    this.a = a;
+    this.b = b;
+    var c = a + b;
+    show(c);
+}
+function show(c){
+    console.log("values: "+c)
+}
+var bv = new myObject(2,2);
+console.log(bv.a);
+console.log(bv.b);
+
+/*,
+### 2. Create objects using literal notation
+Literal notation, sometimes called associative arrays, provides a shorthand way to create objects using JavaScript. Perform these steps:
+
+- Create an object using literal notation.
+- Add a property to the new object.
+- Add a method to the new object.
+*/
+var person =  {
+    firstname : "vamshi",
+    middlename : "krishna",
+    lastname : "hasanabada"
+}
+console.log(person.firstname);
+/*
+### 3. Create objects using the object constructor and prototypes
+The most reusable way to create objects in JavaScript is by using the object constructor paired with prototypes. This method of creating objects is completely flexible, allowing you to create unlimited instances of an object rather than only one. Perform these steps:
+
+- Create an object using the object constructor and prototypes.
+- Add a property to the new object.
+- Add a method to the new object.
+- Instantiate the object.
+*/
+class cell{
+    cell(c){
+        console.log(c)
+    }
+}
+
+var vc = new Object();
+vc.cell("vamshi");
+
+/*
+# Release2: Time to practice some inbuilt function from FreeCodecamp & Learn to write Object Oriented Code
+
+There are lods of in-build library functions to ease your coding. For example. Sort, Map, ToString, etc etc. It's time to brush up your skills on some
+of the most important library functions that you just can't live without in JSWorld
+
+Start [here](https://www.freecodecamp.org/challenges/declare-javascript-objects-as-variables) and finish the exercizes till Joining Strings
+
+# Release3: Shoot yourself in your head or Pat yourself on the back & Let's get started 
+
+It's 50 hours in real time not in  [SOAL](www.soal.io) time! You  can finish this in less than 50 hours. 
+
+So Let's get ready to stay awake for the next 50 hours because these challenges will take away your sleep. Say bye bye to all those happy moments you spend doing nothing and WORK WORK WORK WORK(as Rihana's song says)
+
+- Advanced Algorithm Scripting 
+
+Start [here](https://www.freecodecamp.org/challenges/get-set-for-our-algorithm-challenges) and finish the challenges till Caeser's Cipher*/
